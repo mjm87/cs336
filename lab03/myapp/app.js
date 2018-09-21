f1cd05401abbf265e36beeca08f134aca9c690ba
Michel Momeyer <mjm87@students.calvin.edu>
2018-09-21 12:39:19 -0400

    worked on lab

--- lab03/myapp/app.js
+++ lab03/myapp/app.js
@@ -4,6 +4,8 @@ const port = 3000
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => res.send('Welcome to the main page.'))
 app.get('/hello', (req, res) => res.send('Hello World!'))
+app.get('/goodbye', (req, res) => res.send('Bye World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
