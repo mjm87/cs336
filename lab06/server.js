dded82d4c3178a192147c642080131d483421461
Michel Momeyer <mjm87@students.calvin.edu>
2018-10-12 15:54:12 -0400

    added more

---
+++ lab06/server.js
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+
+const express = require('express');
+const app = express();
+const http_status = require('http-status-codes');
+const bodyParser = require('body-parser');
+
+const HOST = "localhost";
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+ 
+// HTTP route and return code examples.
+
+// GET
+app.get('/requests', function(req, res) {
+    res.status(http_status.OK);
+    res.send("Received: GET");
+});
+
+// HEAD
+app.head('/requests', function(req, res) {
+    res.status(http_status.OK);
+    res.send("Received: HEAD");
+});
+
+// PUT
+app.put('/requests', function(req, res) {
+    res.status(http_status.OK);
+    res.send("Received: PUT " + req.body.data);
+});
+
+// POST
+app.post('/requests', function(req, res) {
+    res.status(http_status.OK);
+    res.send("Received: POST " + req.body.data);
+});
+
+// DELETE
+app.delete('/requests', function(req, res) {
+    res.status(http_status.OK);
+    res.send("Received: DELETE " + req.body.data);
+});
+
+// other unsupported requests
+app.all('/requests', function(req, res) {
+    res.status(http_status.NOT_IMPLEMENTED);
+    res.send("Received unsupported request");
+});
+
+// --------------------------------
+// HTTP form example
+
+// Responds to form posts from the forms/index.html example.
+app.post('/forms', function(req, res) {
+    res.send('Hello, form POST!<br>Posted message: <code>'
+	     + req.body.user_message + '</code>');
+});
+
+// Setting the server up to listen
+app.listen(PORT, HOST, () => {
+    console.log("listening on " + HOST + ":" + PORT + "...");
+});
