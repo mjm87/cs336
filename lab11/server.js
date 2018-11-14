eba1a710b95e1a60c2c7730cdda20c5e88c2c243
Michel Momeyer <mjm87@gold01.cs.calvin.edu>
2018-11-14 14:17:04 -0500

    Started lab11

---
+++ lab11/server.js
@@ -0,0 +1,70 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only. Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var MongoClient = require('mongodb').MongoClient
+var db;
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+app.get('/api/comments', function(req, res) {
+    db.collection('comments').find({}).toArray(function(err, data) {
+        res.json(data);
+    })
+});
+
+app.post('/api/comments', function(req, res) {
+
+    // update the database
+    db.collection('comments').insertOne({
+        id: Date.now(),
+        author: req.body.author,
+        text: req.body.text
+    });
+
+    // re-retrieve the data with the newly inserted comment
+    db.collection('comments').find({}).toArray(function(err, data) {
+        res.json(data);
+    })
+});
+
+// Load the MongoDB comments database
+var mongoURL = 'mongodb://cs336:'+process.env.MONGO_PASSWORD+'@ds253783.mlab.com:53783/cs336';
+MongoClient.connect(mongoURL, function(err, client) {
+    if(err) throw err;
+    db = client;
+    db.collection('comments').find().toArray(function (err, result) {
+        if(err) throw err;
+        // Start listening for clients
+        app.listen(app.get('port'), function() {
+            console.log('Server started: http://localhost:' + app.get('port') + '/');
+        });
+  })
+});
