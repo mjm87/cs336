4fb586e80d9d4c83e756a886ee545c6b07a30ca1
Michel Momeyer <mjm87@gold01.cs.calvin.edu>
2018-11-14 14:17:04 -0500

    Started lab11

---
+++ app/scripts/CommentList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Comment from './Comment.js';
+import '../css/base.css';
+
+// Comment List
+module.exports = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
