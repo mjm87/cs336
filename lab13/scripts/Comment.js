736045bcf6a8e6b14c301b87f669022d8dfb00d2
MichelMomeyer <mjm87@students.calvin.edu>
2018-11-30 10:41:17 -0500

    Added edit option

--- lab13/scripts/Comment.js
+++ lab13/scripts/Comment.js
@@ -17,6 +17,7 @@ module.exports = React.createClass({
             {this.props.author}
             </h2>
             <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            <Link to={'/' + this.props.id}>Edit</Link>
         </div>
         );
     }
